314892531ad24f3d8df1dcc068699695daf8082c
Alex Habich <contact@alexhabi.ch>
2021-05-15 19:01:42 -0500

    Setup fonts

--- users/noobstar/default.nix
+++ users/noobstar/default.nix
@@ -54,4 +54,41 @@ in
       in
       lists.foldr lib.recursiveUpdate {} settings;
   };
+
+
+  # fonts
+  fonts = {
+    enableDefaultFonts = false;
+    fonts = with pkgs; [
+      # sans & serif
+      ttf_bitstream_vera liberation_ttf
+      # monospace
+      fira-code
+      (nerdfonts.override { fonts = [ "FiraCode" ]; })
+      # cjk
+      ipafont
+      # emoji
+      twemoji-color-font
+    ];
+    fontconfig = {
+      defaultFonts = {
+        emoji = [
+          "Twitter Color Emoji"
+          "IPAGothic"
+        ];
+        monospace = [
+          "Fira Code Retina"
+          "IPAGothic"
+        ];
+        sansSerif = [
+          "Bitstream Vera Sans"
+          "IPAGothic"
+        ];
+        serif = [
+          "Bitstream Vera Serif"
+          "IPAGothic"
+        ];
+      };
+    };
+  };
 }
