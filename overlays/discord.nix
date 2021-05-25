6cacf5bd57d9dea87cdb38ccf8ffca2769e305e6
Alex Habich <contact@alexhabi.ch>
2021-05-25 02:33:14 -0500

    Update discord in overlay

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -1,7 +1,12 @@
 self: super:
 
 {
-  discord = super.discord.override {
+  discord = super.discord.override rec {
+    version = "0.0.15";
+    src = super.fetchurl {
+      url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
+      sha256 = "0pn2qczim79hqk2limgh88fsn93sa8wvana74mpdk5n6x5afkvdd";
+    };
     nss = super.nss_latest;
   };
 }
