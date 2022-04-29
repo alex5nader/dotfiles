988045862cbc043a8270ff097f24cd444ccc339f
Alex Habich <contact@alexhabi.ch>
2022-04-29 16:06:47 -0500

    disable wayland

--- gnome.nix
+++ gnome.nix
@@ -3,7 +3,11 @@
 {
   services.xserver = {
     enable = true;
-    displayManager.gdm.enable = true;
+    displayManager.gdm = {
+      enable = true;
+      wayland = false;
+    };
+
     desktopManager.gnome = {
       enable = true;
       extraGSettingsOverrides = ''
