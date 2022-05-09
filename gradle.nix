05898ca5b73de9fecf07217af97ad507158befa9
Alex Habich <contact@alexhabi.ch>
2022-05-09 10:48:39 -0500

    update java 16 to 17

--- gradle.nix
+++ gradle.nix
@@ -4,7 +4,7 @@
   home.file = {
     gradle-properties = {
       text = ''
-        org.gradle.java.home=${pkgs.my-jdks.jdk16}
+        org.gradle.java.home=${pkgs.my-jdks.jdk17}
       '';
       target = ".gradle/gradle.properties";
     };
