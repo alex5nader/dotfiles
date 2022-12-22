91808b3e019d7802fb3deaf112b6942234a0823b
Alex Habich <contact@alexhabi.ch>
2022-12-22 23:41:25 +0900

    enable plymouth

--- system/boot.nix
+++ system/boot.nix
@@ -2,4 +2,9 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.useOSProber = true;
+
+  boot.plymouth = {
+    enable = true;
+  };
 }
+
