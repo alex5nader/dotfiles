0f22db26a41c64b5a2766e81b44ff66583f20ee9
Alex Habich <contact@alexhabi.ch>
2023-08-21 08:48:34 -0500

    switch laptop to chicago time zone

--- system/localization.nix
+++ system/localization.nix
@@ -6,7 +6,7 @@ in
 
 mkMerge [
   (mkIfDevice "laptop" {
-    time.timeZone = "Asia/Tokyo";
+    time.timeZone = "America/Chicago";
 
     i18n.defaultLocale = "en_US.UTF-8";
     services.xserver.layout = "us";
