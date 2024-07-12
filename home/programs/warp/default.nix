eaa2d29fbbdab6daaafcb57b6614a74d6c7d76e3
Alex Habich <contact@alexhabi.ch>
2024-07-12 11:44:15 -0500

    Always warpify subshells

--- home/programs/warp/default.nix
+++ home/programs/warp/default.nix
@@ -10,12 +10,15 @@
     "warp-terminal/launch_configurations/rust.yaml".source = ./launch_configs/rust.yaml;
   };
 
-  # Disable git pager when in warp
   programs.fish.interactiveShellInit = ''
     set -l parent (basename (readlink "/proc/$(cat /proc/$(echo $fish_pid)/stat|cut -d ' ' -f 4)/exe"))
 
     if test "$parent" = warp
+      # Disable git pager
       set -g -x GIT_PAGER
+
+      # Always warpify
+      printf '\eP$f{"hook": "SourcedRcFileForWarp", "value": { "shell": "fish" }}\x9c'
     end
   '';
 }
